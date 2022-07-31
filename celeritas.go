74d8630033cd54891a74f48476e2f375d2025b47
Maurice Wittek <maurice.wittek@gmail.com>
2022-07-31 11:05:04 +0200

    adds support for jet templates

--- celeritas.go
+++ celeritas.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/CloudyKit/jet/v6"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 	"github.com/mauricewittek/celeritas/render"
@@ -24,6 +25,7 @@ type Celeritas struct {
 	RootPath string
 	Routes   *chi.Mux
 	Render   *render.Render
+	JetViews *jet.Set
 	config   config
 }
 
@@ -74,6 +76,13 @@ func (c *Celeritas) New(rootPath string) error {
 		port:     os.Getenv("PORT"),
 		renderer: os.Getenv("RENDERER"),
 	}
+
+	var views = jet.NewSet(
+		jet.NewOSFileSystemLoader(fmt.Sprintf("%s/views", rootPath)),
+		jet.InDevelopmentMode(),
+	)
+
+	c.JetViews = views
 
 	c.createRenderer()
 
@@ -132,6 +141,7 @@ func (c *Celeritas) createRenderer() {
 		Renderer: c.config.renderer,
 		RootPath: c.RootPath,
 		Port:     c.config.port,
+		JetViews: c.JetViews,
 	}
 
 	c.Render = &myRenderer
