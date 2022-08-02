90149a94e509187188a9a946659731061e3b9e56
Maurice Wittek <maurice.wittek@gmail.com>
2022-08-02 08:02:02 +0200

    adds tests for render functions

--- render/render.go
+++ render/render.go
@@ -1,6 +1,7 @@
 package render
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
@@ -37,9 +38,10 @@ func (c *Render) Page(w http.ResponseWriter, r *http.Request, view string, varia
 		return c.GoPage(w, r, view, data)
 	case "jet":
 		return c.JetPage(w, r, view, variables, data)
+	default:
 	}
 
-	return nil
+	return errors.New("No rendering engine specified")
 }
 
 func (c *Render) GoPage(w http.ResponseWriter, r *http.Request, view string, data interface{}) error {
---
+++ render/render_test.go
@@ -0,0 +1,79 @@
+package render
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var pageData = []struct {
+	name          string
+	renderer      string
+	template      string
+	errorExpected bool
+	errorMessage  string
+}{
+	{"go_page", "go", "home", false, "error rendering go template"},
+	{"go_page_no_template", "go", "no-file", true, "expected an error rendering non existent go template but got none"},
+	{"jet_page", "jet", "home", false, "error rendering jet template"},
+	{"jet_page_no_template", "jet", "no-file", true, "expected an error rendering non existent jet template but got none"},
+	{"invalid_render_engine", "", "home", true, "expected an error rendering non existent template engine but got none"},
+}
+
+func TestRender_Page(t *testing.T) {
+
+	for _, e := range pageData {
+		r, err := http.NewRequest("GET", "/some-url", nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		w := httptest.NewRecorder()
+
+		testRenderer.Renderer = e.renderer
+		testRenderer.RootPath = "./testdata"
+
+		err = testRenderer.Page(w, r, e.template, nil, nil)
+		if e.errorExpected {
+			if err == nil {
+				t.Errorf("%s: %s:", e.name, e.errorMessage)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%s: %s: %s:", e.name, e.errorMessage, err.Error())
+			}
+		}
+	}
+}
+
+func TestRender_GoPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "go"
+	testRenderer.RootPath = "./testdata"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Error rendering page", err)
+	}
+}
+
+func TestRender_JetPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "jet"
+	testRenderer.RootPath = "./testdata"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Error rendering page", err)
+	}
+}
